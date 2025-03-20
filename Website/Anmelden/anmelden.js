f2e67426e613d0c31e7b0ea78f362f65b1c4a66e
Jonas <jonas.hutzler@bs-erlangen.de>
2025-03-20 09:43:35 +0100

    WIP
    WIP

--- Website/Anmelden/anmelden.js
+++ Website/Anmelden/anmelden.js
@@ -1,20 +1,25 @@
-const { getPasswordFromDb } = require('../Datenbank/datenbank.js');
-
-document.getElementById('submit').addEventListener('click', function(event) {
-    event.preventDefault(); // Verhindert das Absenden des Formulars	
+document.getElementById('submit').addEventListener('click', function(event) {	
 
     // Werte aus den Eingabefeldern holen
-    
-    var inputPassword = document.getElementById('password').value;    
-    getPasswordFromDb();
+    var inputPassword = document.getElementById('password').value;
+    var inputEmail = document.getElementById('email').value;
+
+    getPasswordFromDb(inputEmail, inputPassword, event);
+    document.getElementById('screen').submit();
 });
 
-async function getPasswordFromDb() {
-    var inputEmail = "max.mustermann@example.com";
-    const response = await fetch('http://localhost:3000/passwort/${inputEmail}');
+async function getPasswordFromDb(inputEmail, inputPassword, event) {
+    const response = await fetch(`http://localhost:3000/passwort?email=${inputEmail}`);
     const userData = await response.json();
+    const passwordDb = userData[0].selection;
+    console.log(passwordDb);
     console.log(userData);
-    return userData;
+    
+    if (vergleicheEingaben(inputPassword, passwordDb, event)) {
+        document.getElementById('screen').submit();
+    } else {
+        event.preventDefault(); // Verhindert das Absenden des Formulars
+    }
 }
 
 function vergleicheEingaben(inputPassword, passwordDb) {
--- Website/Registrieren/registrieren.js
+++ Website/Registrieren/registrieren.js
@@ -1,6 +1,8 @@
 const submitButton = document.getElementById('submit');
 
-submitButton.onclick = async function hashString() {
+submitButton.onclick = async function hashString(event) {
+    event.preventDefault();
+
     const encoder = new TextEncoder();
     const data = encoder.encode(document.getElementById('password').value);
     const hashBuffer = await crypto.subtle.digest('SHA-256', data);
@@ -8,5 +10,25 @@ submitButton.onclick = async function hashString() {
     // Buffer in Hex-String umwandeln
     const hashArray = Array.from(new Uint8Array(hashBuffer));
     const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-    alert(hashHex);
+
+    // Daten an den Server senden
+    await registrieren(hashHex);
+}
+
+async function registrieren(hashHex) {
+    const vorname = document.getElementById('vorname').value;
+    const nachname = document.getElementById('name').value;
+    const geschlecht = document.getElementById('geschlecht').value;
+    const geburtsdatum = document.getElementById('geburtsdatum').value;
+    const ort = document.getElementById('stadt').value;
+    const straße = document.getElementById('straße').value;
+    const plz = document.getElementById('plz').value;
+    const hausnummer = document.getElementById('hausnummer').value;
+    const email = document.getElementById('email').value;
+    const anrede = document.getElementById('anrede').value;
+
+    const url = `http://localhost:3000/kunde?vorname=${vorname}&nachname=${nachname}&geschlecht=${geschlecht}&geburtsdatum=${geburtsdatum}&ort=${ort}&straße=${straße}&plz=${plz}&hausnummer=${hausnummer}&email=${email}&passwort=${hashHex}&gast=${0}&anrede=${anrede}`;
+    console.log(url);
+    const response = await fetch(url);
+    const result = await response.json();
 }
